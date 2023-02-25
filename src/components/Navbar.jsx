3bf2f21e9a2d984cb2f07967f735f209fa312fd3
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-02-25 17:23:21 -0500

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import "./Navbar.scss";
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <h2> Openai project</h2>
+      <ul className="features">
+        <li>
+          <a href="/generate">Generate Image</a>
+        </li>
+        <li>
+          <a href="/edit">Edit Image</a>
+        </li>
+      </ul>
+    </div>
   )
 }
