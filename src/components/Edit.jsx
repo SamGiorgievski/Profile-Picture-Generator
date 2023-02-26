a58847ed84a45e627e03af82edd6414f74694809
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-02-26 17:57:47 -0500

    image upload

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import {useState} from 'react';
 import "./Edit.scss";
 
 export default function Edit() {
+
+  const [pic, setPic] = useState({
+    selectedFile: null,
+    imagePath: null
+  });
+
+  const fileSelectedHandler = (event) => {
+    const imagePath = URL.createObjectURL(event.target.files[0]);
+
+    setPic({
+      selectedFile: event.target.files[0],
+      imagePath: {imagePath}
+    })
+    console.log(event.target.files[0]);
+  }
+  
+  // const fileUploadHandler = (event) => {
+  //   URL.createObjectURL(pic.selectedFile)
+  // }
+
   return (
     <div className="edit">
-      Edit
+      <input type="file" onChange={fileSelectedHandler}></input>
+      {/* <button onClick={fileUploadHandler}>Upload</button> */}
+      {pic.imagePath && <img src={pic.imagePath} alt="Upload" />}
     </div>
   )
 }
